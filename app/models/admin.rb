6b132a461eccab6888851146723c2240eeee1fe2
lucasvidela94 <lucas.videla994@gmail.com>
2022-07-04 20:35:25 -0300

    Add devise basic structure
    Co-authored-by: guillermo-delprado <galfonso.delprado@gmail.com>
    Co-authored-by: HikingCarrot7 <ricardoibarra2044@gmail.com>

---
+++ app/models/admin.rb
@@ -0,0 +1,9 @@
+class Admin < ApplicationRecord
+  before_commit :authenticate_user!
+
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
