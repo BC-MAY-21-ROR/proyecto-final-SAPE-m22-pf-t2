f174ef4c9292e6ebc4c5f73b2663b7f534a4a14c
guillermo-delprado <galfonso.delprado@gmail.com>
2022-07-04 19:09:27 -0500

    Fix some devise routes
    Co-authored-by: HikingCarrot7 <ricardoibarra2044@gmail.com>
    Co-authored-by: lucasvidela94 <lucas.videla994@gmail.com>

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,4 @@
 class Admin < ApplicationRecord
-  before_commit :authenticate_user!
-
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
