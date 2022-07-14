a6980094e39e142c7842c54c4464f83e93b0b2ec
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-07-14 17:37:55 -0500

    Added simple validations for business entity

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,10 @@
 class Business < ApplicationRecord
   belongs_to :admin
   belongs_to :country
+
+  validates :name, :business_type, :address, presence: true
+
+  validates :name, length: { minimum: 5 }
+  validates :business_type, length: { minimum: 3 }
+  validates :address, length: { minimum: 5 }
 end
