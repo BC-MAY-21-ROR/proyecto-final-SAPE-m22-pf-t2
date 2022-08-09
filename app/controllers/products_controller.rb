8ec3a2d23f5768986e6231189f977ce7d088a5de
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-08-08 23:09:32 -0500

    Improved product new & edit views styling

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,7 +19,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to product_url(@product), notice: 'Product was successfully created.' }
+        format.html { redirect_to products_path, notice: 'Product was successfully created.' }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
@@ -29,7 +29,7 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to product_url(@product), notice: 'Product was successfully updated.' }
+        format.html { redirect_to products_path, notice: 'Product was successfully updated.' }
       else
         format.html { render :edit, status: :unprocessable_entity }
       end
