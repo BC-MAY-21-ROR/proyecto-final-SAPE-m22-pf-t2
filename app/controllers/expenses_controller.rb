9721f71a418068671a39b840fb363ad8c3e34441
lucasvidela94 <lucas.videla994@gmail.com>
2022-08-12 19:37:39 -0300

    Add first approach to set only one expense per month Organized views for expense

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,6 +2,7 @@ class ExpensesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_expense, only: %i[show edit update destroy]
 
+
   def index
     @expenses = Expense.all
   end
@@ -15,16 +16,37 @@ class ExpensesController < ApplicationController
   def edit; end
 
   def create
-    @expense = Expense.new(expense_params.merge({ business: current_business }))
+
     
+    @expense = Expense.new(expense_params.merge({ business: current_business }))
     respond_to do |format|
       if @expense.save
-        format.html { redirect_to expense_url(@expense), notice: 'Expense was successfully created.' }
+        format.html { redirect_to expense_url, notice: 'Expense was successfully created.' }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
     end
   end
+
+  def first_expense
+    unless @expenses.nil?
+      @first_expense = First(@expenses)
+    else
+      format.html { , status: :unprocessable_entity }
+  end
+
+  def check_expense_month
+    current_expense = First(Expense.find_by(month: current_expense.month))
+    @expenses.each do |i|
+      if i.month !== current_expense.month
+        return true
+      else
+        flash[:alert]  = "This expense is repeated"
+        returns false
+      end
+    end
+  end
+
 
   def update
     respond_to do |format|
@@ -50,6 +72,11 @@ class ExpensesController < ApplicationController
     @expense = Expense.find(params[:id])
   end
 
+  def set_limit 
+    @expenses
+  end
+
+ 
   def expense_params
     puts params
     params.require(:expense).permit(
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,4 +2,5 @@ class Expense < ApplicationRecord
   belongs_to :business
   validates :rent, :salaries, :general_charges, :service_bills, :commissions, :taxes,
             numericality: { greater_than_or_equal_to: 0 }
+          
 end
